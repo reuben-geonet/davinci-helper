bab7068e6c07a2e159f348725e0b22a62d01a1f0
reuben-geonet <r.andrews@gns.cri.nz>
2026-04-08 14:16:32 +1200

    nfc: remove legacy bash scripts

--- create_archive.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-clear
-version="2.4.7"
-
-mv davinci-helper davinci-helper-${version}
-tar -cvzf davinci-helper-${version}.tar.gz davinci-helper-${version}
-mv davinci-helper-${version} davinci-helper
